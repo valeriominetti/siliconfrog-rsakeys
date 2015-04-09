6de9be9ef2e0ac3d7397fde700f4dad33c5eb73a
Valerio Minetti <valeriominetti@users.noreply.github.com>
2015-04-09 11:47:10 +0200

    updated user management

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,13 @@
 #
 # Parameters: $user (user name)
 #             $rsa_key_hash (hash array containing rsa keys)
+#             hiera structure example
+#
+#             rsa_keys_array:
 #                keylabel1:
-#                    - key: <put your rsa pub key here>      
+#                    - key: <put your rsa pub key here>
 #                 keylabel2:
-#                    - key: <put your rsa pub key here>      
+#                    - key: <put your rsa pub key here>
 #             $purge (purge existent rsa keys from user if true)
 #
 # Actions:
@@ -21,32 +24,37 @@
 #               }
 #
 
-class rsa_keys ($user='root', $rsa_key_hash= '', $purge= true){
+class rsa_keys ($user='root', $rsa_key_hash='', $purge=true){
 ##
 #
-# Check user and purge ssh key if requested 
+# Check user and purge ssh key if requested
 #
 ##
-if ($user = 'root'){$homepath='/root' } else {$homepath='/home/$user' }
+if $user=='root' {
     user { $user:
                 ensure         => present,
-                home           => $homepath,
-                #todo check user uid if not root.
-                uid            => '0', 
+                home           => '/root',
+                uid            => '0',
                 purge_ssh_keys => $purge,
         }
+} else {
+  user { $user:
+          ensure         => present,
+          home           => '/home/$user',
+          purge_ssh_keys => $purge,
+  }
+}
 ##
 #
 # Add all root rsa keys from hiera
 #
 ##
 
-        $defaults = {
-                ensure => 'present',
-                user   => 'root',
-                type   => 'ssh-rsa',
-        }
-
-  #create_resources(ssh_authorized_key, hiera_hash('root_pub_key'), $defaults)
-  create_resources(ssh_authorized_key, $rsa_key_hash, $defaults)
+$defaults = {
+  ensure => 'present',
+  user   => 'root',
+  type   => 'ssh-rsa',
+}
+#create_resources(ssh_authorized_key, hiera_hash('root_pub_key'), $defaults)
+create_resources(ssh_authorized_key, $rsa_key_hash, $defaults)
 }
